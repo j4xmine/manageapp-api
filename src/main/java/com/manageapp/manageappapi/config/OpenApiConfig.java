6bfe410424ec38f84fbeb71003e2b2a9791b910f
j4xmine <stontato.victor@gmail.com>
2026-06-13 12:59:59 -0300

    Swagger: adicionado a classe OpenApiConfig com informações de contato

--- src/main/java/com/manageapp/manageappapi/config/OpenApiConfig.java
+++ src/main/java/com/manageapp/manageappapi/config/OpenApiConfig.java
@@ -1,4 +1,28 @@
 package com.manageapp.manageappapi.config;
 
-public class OpenApiConfig {
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig { // descrição no início da página do swagger com informações e contato comigo.
+
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI().info(
+                new Info()
+                        .title("ManageApp API")
+                        .description("Sistema de Gerenciamento de Produtos desenvolvido com Spring Boot")
+                        .version("1.0.0")
+                        .contact(
+                                new Contact()
+                                        .name("Rodrigues")
+                                        .email("stcontato.victor@gmail.com")
+                        )
+        );
+    }
+
 }
