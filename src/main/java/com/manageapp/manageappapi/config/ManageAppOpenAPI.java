a4291fee32fbe01500fbef205fcc8ea957c7ebbe
j4xmine <stontato.victor@gmail.com>
2026-06-13 13:04:36 -0300

    ManageAppOpenAPI: alteração da descrição

--- src/main/java/com/manageapp/manageappapi/config/ManageAppOpenAPI.java
+++ src/main/java/com/manageapp/manageappapi/config/ManageAppOpenAPI.java
@@ -8,14 +8,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ManageAppOpenApi { // descrição no início da página do swagger com informações e contato comigo.
+public class ManageAppOpenAPI { // descrição no início da página do swagger com informações e contato comigo.
 
     @Bean
     public OpenAPI customOpenApi() {
         return new OpenAPI().info(
                 new Info()
                         .title("ManageApp API")
-                        .description("Sistema de Gerenciamento de Produtos desenvolvido com Spring Boot")
+                        .description("API REST desenvolvida em Spring Boot para Gerenciamento de Produtos e Controle de Estoque.")
                         .version("1.0.0")
                         .contact(
                                 new Contact()
